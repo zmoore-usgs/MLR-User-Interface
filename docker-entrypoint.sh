7cb8430d7f109a657d3e8a50c523e89d35b2f77c
Stanley Kaymen <skaymen@usgs.gov>
2017-11-03 14:51:51 -0500

    MLR-208 Modify application to take PEM and CRT

---
+++ docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -x
+
+keystorePassword=`cat $KEYSTORE_PASSWORD_FILE`
+
+openssl pkcs12 -export -in $TOMCAT_CERT_path -inkey $TOMCAT_KEY_path -name tomcat -out tomcat.p12
+
+keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore $keystoreLocation -srckeystore tomcat.p12 -srcstoretype PKCS12
+
+if [ $use_doi_cert = true ] ; then curl -o root.crt http://sslhelp.doi.net/docs/DOIRootCA2.cer ; fi
+if [ $use_doi_cert = true ] ; then keytool  -importcert -file root.crt -alias doi -keystore $keystoreLocation -storepass $keystorePassword -noprompt; fi
+
+if [ -n "$samlIdpHost" ] ; then openssl s_client -host $samlIdpHost -port $samlIdpPort -prexit -showcerts </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > samlidp.crt; fi
+if [ -n "$samlIdpHost" ] ; then keytool  -importcert -file samlidp.crt -alias samlidp -keystore $keystoreLocation -storepass $keystorePassword -noprompt; fi
+
+keytool -list -keystore $keystoreLocation -storepass $keystorePassword
+java -Djava.security.egd=file:/dev/./urandom -DkeystorePassword=$keystorePassword -jar app.jar
+
+exec $?
