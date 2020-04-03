9f605c1c73cddb0a4e884a9a2eca4720e07c481d
Dominick Pattermann <dpattermann@usgs.gov>
2020-04-03 10:53:49 -0500

    partway UI to add dialog boxes and add validate/upload functionality to buttons

--- src/components/DdotProcessCard.vue
+++ src/components/DdotProcessCard.vue
@@ -7,9 +7,29 @@
             </v-form>
         </v-card-text>
         <v-card-actions>
-            <v-btn class="mx-2" color="primary" @click="validateFile">Validate</v-btn>
-            <v-btn color="primary">Validate and Update records</v-btn>
+            <v-btn class="mx-2" color="primary" @click="validateButtonClicked">Validate</v-btn>
+            <v-btn color="primary" @click="uploadButtonClicked">Validate and Update records</v-btn>
         </v-card-actions>
+        <v-dialog v-model="showValidateDialog">
+            <v-card>
+                <v-card-title>Validate</v-card-title>
+                <v-card-text>{{confirmStatement}}</v-card-text>
+                <v-card-actions>
+                    <v-btn color="primary" class="mx-2" @click="showValidateDialog = false">Close</v-btn>
+                    <v-btn color="primary" @click="validateDdotFile">Upload</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+        <v-dialog v-model="showUploadDialog">
+            <v-card>
+                <v-card-title>Upload</v-card-title>
+                <v-card-text>{{confirmStatement}}</v-card-text>
+                <v-card-actions>
+                    <v-btn color="primary" class="mx-2" @click="showUploadDialog = false">Close</v-btn>
+                    <v-btn color="primary" @click="uploadDdotFile">Upload</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </v-card>
 </template>
 
@@ -21,12 +41,62 @@ export default {
 
     data() {
         return {
-            ddotFile: null
+            ddotFile: null,
+            districtCodes: null,
+            transactionTypes: null,
+            showValidateDialog: false,
+            showUploadDialog: false
         };
     },
+    computed: {
+        confirmStatement() {
+            var toRet = this.transactionTypesCheck
+                ? "Please coordinate with other LDMs if you are creating a site in another district code.\n"
+                : "";
+            toRet += this.districtCodesCheck
+                ? "Please be aware that you are adding or modifying sites in multiple district codes."
+                : "";
+            return toRet;
+        },
+        transactionTypesCheck() {
+            return this.transactionTypes && this.transactionTypes.includes("A");
+        },
+        districtCodesCheck() {
+            return this.districtCodes && this.districtCodes.length > 1;
+        }
+    },
     methods: {
-        validateFile() {
-            DdotApi.postDdot("util/parse", this.ddotFile).then(response => {
+        validateButtonClicked() {
+            this.parseDdotFile().then(() => {
+                if (this.transactionTypesCheck || districtCodesCheck) {
+                    this.showValidateDialog = true;
+                } else {
+                    this.validateDdotFile();
+                }
+            });
+        },
+        uploadButtonClicked() {
+            this.parseDdotFile().then(() => {
+                if (this.transactionTypesCheck || districtCodesCheck) {
+                    this.showUploadDialog = true;
+                } else {
+                    this.uploadDdotFile();
+                }
+            });
+        },
+        parseDdotFile() {
+            return DdotApi.parseDdot(this.ddotFile).then(response => {
+                this.districtCodes = response.districtCodes;
+                this.transactionTypes = response.transactionTypes;
+            });
+        },
+        uploadDdotFile() {
+            DdotApi.uploadDdot(this.ddotFile).then(response => {
+                console.log(response);
+            });
+        },
+        validateDdotFile() {
+            DdotApi.validateDdot(this.ddotFile).then(response => {
                 console.log(response);
             });
         }
--- src/services/api/DdotApi.js
+++ src/services/api/DdotApi.js
@@ -1,6 +1,15 @@
 import axios from 'axios'
 
 export default {
+	parseDdot(ddotFile) {
+		return this.postDdot("util/parse", ddotFile);
+	},
+	uploadDdot(ddotFile) {
+		return this.postDdot("workflows/ddots", ddotFile);
+	},
+	validateDdot(ddotFile) {
+		return this.postDdot("workflows/ddots/validate", ddotFile);
+	},
 	postDdot(url, ddotFile) {
 		var ddotData = new FormData();
 		ddotData.append("file", ddotFile);
