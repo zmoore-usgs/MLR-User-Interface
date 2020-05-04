b7b395bbab11fd7a88fca51b3cd3c2bce61f0680
Carl Schroedl <cschroedl@usgs.gov>
2020-05-04 15:39:28 -0500

    simplified

--- src/components/MLRFooter.vue
+++ src/components/MLRFooter.vue
@@ -3,10 +3,10 @@
         <v-row :align="'center'">
             <v-col class="text-center">
                 For write access to MLR, email
-                <v-btn  href="mailto:gs-w_help_nwis-ts@usgs.gov?cc=YOUR_LDM_HERE@usgs.gov&subject=MLR%20Write%20Access&body=Please%20change%20the%20CC%20field%20so%20that%20it%20contains%20the%20email%20address%20of%20your%20local%20data%20manager." title="Request Write Access" text>
-                    <span class="mr-2"> gs-w_help_nwis-ts@usgs.gov</span>
+                <a href="mailto:gs-w_help_nwis-ts@usgs.gov?cc=YOUR_LDM_HERE@usgs.gov&subject=MLR%20Write%20Access&body=Please%20change%20the%20CC%20field%20so%20that%20it%20contains%20the%20email%20address%20of%20your%20local%20data%20manager." title="Request Write Access" text>
+                    gs-w_help_nwis-ts@usgs.gov
                     <v-icon>mdi-email</v-icon>
-                </v-btn>
+                </a>
                  and CC your local data manager.
             </v-col>
         </v-row>
