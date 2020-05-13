ff77f1d5b1d03073e3c2627ca85952368c7684e0
Kathryn Schoephoester <kmschoep@usgs.gov>
2020-05-13 14:51:17 -0500

    progress on parsing

--- src/components/UpdatePrimaryKeyCard.vue
+++ src/components/UpdatePrimaryKeyCard.vue
@@ -2,57 +2,51 @@
     <v-card flat>
         <v-card-title>Station Change</v-card-title>
         <v-card-text>
-            <v-form
-                ref="stationChangeForm"
-                v-model="validForm"
-                lazy-validation
-                >
-                <v-row>
-                     <v-col cols="2" sm="6" md="4">
-                        <v-text-field
-                            v-model="oldAgencyCode"
-                            :rules="requiredRules"
-                            label="Agency Code"
-                            required
-                        ></v-text-field>
-                     </v-col>
-                     <v-col cols="2" sm="6" md="6">
-                        <v-text-field
-                            v-model="oldSiteNumber"
-                            :rules="requiredRules"
-                            label="Site Number"
-                            required
-                        ></v-text-field>
-                     </v-col>
-                </v-row>
-                <v-row>
-                     <v-col cols="2" sm="6" md="4">
-                        <v-text-field
-                            v-model="newAgencyCode"
-                            :rules="requiredRules"
-                            label="New Agency Code"
-                            required
-                        ></v-text-field>
-                     </v-col>
-                    <v-col cols="2" sm="6" md="6">
-                        <v-text-field
-                            v-model="newSiteNumber"
-                            :rules="requiredRules"
-                            label="New Site Number"
-                            required
-                        ></v-text-field>
+            <v-row>
+                    <v-col cols="2" sm="6" md="4">
+                    <v-text-field
+                        v-model="oldAgencyCode"
+                        :rules="requiredRules"
+                        label="Agency Code"
+                        required
+                    ></v-text-field>
                     </v-col>
-                </v-row>
-                <v-row>
-                <v-text-field
-                    v-model="reasonText"
-                    :counter="64"
-                    :rules="validReasonText"
-                    label="Reason for change"
-                    required
-                ></v-text-field>
-                </v-row>
-                </v-form>
+                    <v-col cols="2" sm="6" md="6">
+                    <v-text-field
+                        v-model="oldSiteNumber"
+                        :rules="requiredRules"
+                        label="Site Number"
+                        required
+                    ></v-text-field>
+                    </v-col>
+            </v-row>
+            <v-row>
+                    <v-col cols="2" sm="6" md="4">
+                    <v-text-field
+                        v-model="newAgencyCode"
+                        :rules="requiredRules"
+                        label="New Agency Code"
+                        required
+                    ></v-text-field>
+                    </v-col>
+                <v-col cols="2" sm="6" md="6">
+                    <v-text-field
+                        v-model="newSiteNumber"
+                        :rules="requiredRules"
+                        label="New Site Number"
+                        required
+                    ></v-text-field>
+                </v-col>
+            </v-row>
+            <v-row>
+            <v-text-field
+                v-model="reasonText"
+                :counter="64"
+                :rules="validReasonText"
+                label="Reason for change"
+                required
+            ></v-text-field>
+            </v-row>
         </v-card-text>
         <v-card-actions>
             <v-btn color="primary" @click="updatePrimaryKey">Station Change</v-btn>
@@ -95,17 +89,136 @@ export default {
     },
     methods: {
         updatePrimaryKey() {
-            this.validForm = this.$refs.stationChangeForm.validate();
-            if(this.validForm) {
-                LegacyLocationApi.postChange(this.oldAgencyCode, this.oldSiteNumber, this.newAgencyCode, this.newSiteNumber, this.reasonText)
-                    .then(response => {
-                        this.$emit("change-workflow", response.data);
-                    })
-                    .catch(error => {
-                        this.$emit("change-workflow", response.data);
-                    });
-
+            LegacyLocationApi.postChange(this.oldAgencyCode, this.oldSiteNumber, this.newAgencyCode, this.newSiteNumber, this.reasonText)
+                .then(response => {
+                    this.handleWorkflowError(response);
+                })
+                .catch(error => {
+                    this.handleWorkflowError(error.response);
+                    console.log(error);
+                });
+        },
+        parseMessage(message){
+            if (message.includes("_message") || (message.includes("validation_errors"))){
+                return JSON.parse(message);
+            } else {
+                return message;
             }
+        },
+        handleWorkflowError(response) {
+            var workflowFailureMsg = {};
+            var workflowErrors = null;
+            if (response.data.workflowSteps.length > 0) {
+                var workflowFailure = response.data.workflowSteps.filter(
+                    function(x) {
+                        return x.name.search("Workflow") > 0;
+                    }
+                );
+                if (workflowFailure.length > 0) {
+                    workflowFailureMsg.workflowStatus = {
+                        name: workflowFailure[0].name,
+                        message:
+                            this.parseMessage(workflowFailure[0].details)
+                    };
+                }
+                workflowErrors = response.data.workflowSteps.filter(function(
+                    x
+                ) {
+                    return x.name.search("Workflow") < 0;
+                });
+            }
+            if (workflowFailureMsg.workflowStatus === undefined) {
+                workflowFailureMsg.workflowStatus = {
+                    name: "Status",
+                    message:
+                        response.data.numberSiteSuccess +
+                        " Transactions Succeeded, " +
+                        response.data.numberSiteFailure +
+                        " Transactions Failed"
+                };
+            }
+            if (workflowErrors != null) {
+                workflowFailureMsg.workflowLevelErrors = {
+                    name: "Workflow-level Errors",
+                    errors: []
+                };
+                workflowErrors.forEach(function(e) {
+                    workflowFailureMsg.workflowLevelErrors.errors.push({
+                        name: e.name,
+                        message: JSON.parse(e.details).error_message
+                    });
+                });
+            }
+            if (response.data.sites.length > 0) {
+                workflowFailureMsg.siteErrors = {
+                    name: "Site-level Errors and Warnings",
+                    errors: this.parseSiteErrorRows(response.data.sites)
+                };
+            }
+            this.$emit("changeWorkflow", response.data, workflowFailureMsg);
+        },
+        parseSiteErrorRows(errorList) {
+            var result = [];
+            errorList.forEach(function(s) {
+                var site = s.agencyCode.trim() + "-" + s.siteNumber.trim();
+                var steps = s.steps;
+                steps.forEach(function(st) {
+                    var detailMsg = this.parseMessage(st.details);
+                    if (detailMsg.hasOwnProperty("error_message")) {
+                        if (typeof this.parseMessage(detailMsg.error_message) === "object") {
+                            var detailErrMessage = this.parseMessage(detailMsg.error_message)
+                            if (
+                                detailErrMessage.hasOwnProperty("validation_errors")
+                                ) {
+                                    Object.keys(
+                                        detailErrMessage.validation_errors
+                                    ).forEach(function(key) {
+                                        result.push({
+                                            name: site,
+                                            message:
+                                                st.name +
+                                                " Fatal Error: " +
+                                                key +
+                                                " - " +
+                                                detailErrMessage.validation_errors[key]
+                                        });
+                                    });
+                                }
+                        } else {
+                            result.push({
+                                name: site,
+                                message:
+                                    st.name +
+                                    " Fatal Error: " +
+                                    detailMsg.error_message
+                            });
+                        }
+                    }
+                    if (detailMsg.hasOwnProperty("validator_message")) {
+                        if (
+                            detailMsg.validator_message.hasOwnProperty(
+                                "fatal_error_message"
+                            )
+                        ) {
+                            Object.keys(
+                                detailMsg.validator_message.fatal_error_message
+                            ).forEach(function(key) {
+                                result.push({
+                                    name: site,
+                                    message:
+                                        st.name +
+                                        " Fatal Error: " +
+                                        key +
+                                        " - " +
+                                        detailMsg.validator_message
+                                            .fatal_error_message[key]
+                                });
+                            });
+                        }
+                    }
+                }.bind(this));
+            }.bind(this));
+            return result;
         }
     }
 };
--- src/components/UpdatePrimaryKeyReport.vue
+++ src/components/UpdatePrimaryKeyReport.vue
@@ -2,18 +2,23 @@
     <v-container>
         <span v-if="report.numberSiteSuccess == 1">Station Change Successful</span>
         <span v-if="report.numberSiteSuccess == 0">Station Change Errors:</span>
-        <v-list v-if="showWorkflowErrors">
-            <v-list-item v-for="(error, i) in report.workflowSteps" :key="i">
-                <v-list-item-content>{{error.name}}: {{parsedErrorMessage(error.details)}}</v-list-item-content>
-            </v-list-item>
-        </v-list>
-        <v-list v-if="showSiteErrors">
-            <v-list-item v-for="(error, i) in report.sites[0].steps" :key="i">
-                <v-list-item v-for="(message, j) in parsedErrorResponse(error.details)" :key="j">
-                    <v-list-item-content>{{error.name}}: {{message}}</v-list-item-content>
+        <div>
+        <p v-if="report.workflowStatus">{{report.workflowStatus.name}}: {{report.workflowStatus.message}}</p>
+
+            <v-list v-if="report.workflowLevelErrors">
+                <v-list-item-subtitle>{{report.workflowLevelErrors.name}}</v-list-item-subtitle>
+                <v-list-item v-for="(error, i) in report.workflowLevelErrors.errors" :key="i">
+                    <v-list-item-content>{{error.name}} {{error.message}}</v-list-item-content>
                 </v-list-item>
-            </v-list-item>
-        </v-list>
+            </v-list>
+
+            <v-list v-if="report.siteErrors">
+                <v-list-item-subtitle>{{report.siteErrors.name}}</v-list-item-subtitle>
+                <v-list-item v-for="(error, i) in report.siteErrors.errors" :key="i">
+                    <v-list-item-content>{{error.name}} {{error.message}}</v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </div>
     </v-container>
 </template>
 <script>
@@ -27,26 +32,6 @@ export default {
         },
         showWorkflowErrors(){
             return this.report.workflowSteps.length > 0;
-        }
-    },
-    methods: {
-        parsedErrorResponse(details){
-            var error_message;
-            var validation_errors;
-            var error_messages = [];
-            error_message = this.parsedErrorMessage(details);
-            if (error_message.includes('validation_errors')){
-                validation_errors = JSON.parse(error_message);
-                _.forIn(validation_errors.validation_errors, function(value, key){
-                    error_messages.push(value);
-                })
-            } else {
-                error_messages.push(error_message);
-            }
-            return error_messages;
-        },
-        parsedErrorMessage(details){
-            return JSON.parse(details).error_message;
         }
     }
 };
