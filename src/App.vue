6b6a0b82c284e5a57fd8d7d5b3ba56c053591ac4
Kathryn Schoephoester <kmschoep@usgs.gov>
2020-05-21 08:29:32 -0500

    undo my merge snafu

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,6 @@
 <template>
     <v-app>
         <USGSHeaderBar />
-        <v-content>
-            <v-row>
-                <v-col>
-                    <DdotProcessCard @validateWorkflow="setReportData" />
-                </v-col>
-                <v-divider vertical color="black"></v-divider>
-                <v-col>
-                    <CopyLocationCard @exportWorkflow="setReportData" />
-                </v-col>
-                <v-divider vertical color="black"></v-divider>
-                <v-col v-if="showStationChange">
-                    <UpdatePrimaryKeyCard @changeWorkflow="setReportData" />
-                </v-col>
-            </v-row>
-            <v-card v-if="responseData">
-                <v-list dense>
-                    <v-list-item>MLR Workflow: {{responseData.name}}</v-list-item>
-                    <v-list-item>User: {{responseData.userName}}</v-list-item>
-                    <v-list-item>Date: {{responseData.reportDateTime}}</v-list-item>
-                    <v-list-item>Input File: {{handleNullAttributes(responseData.inputFileName)}}</v-list-item>
-                    <v-list-item v-if="exportReport">
-                        <ExportReport :report="exportReport" />
-                    </v-list-item>
-                    <v-list-item v-if="validateReport">
-                        <ValidateReport :report="validateReport" />
-                    </v-list-item>
-                    <v-list-item v-if="updatePrimaryKeyReport">
-                        <UpdatePrimaryKeyReport :report="updatePrimaryKeyReport" />
-                    </v-list-item>
-                    <v-list-item>
-                        <v-btn
-                            text
-                            color="primary"
-                            @click="downloadStepReport"
-                        >Download the Error Warning/Step Report</v-btn>
-                    </v-list-item>
-                </v-list>
-            </v-card>
-        </v-content>
         <keep-alive>
             <router-view></router-view>
         </keep-alive>
